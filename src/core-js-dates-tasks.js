d38158f287632585d15fcadc1c5bbba0a63e034e
Olga <c_h_e_r_r_y@tut.by>
2024-12-28 23:04:55 +0300

    feat: tasks getTime, getDayName, namesDayArr, getNextFriday, getCountDaysInMonth

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,8 +31,8 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  return `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
 }
 
 /**
@@ -46,8 +46,17 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const namesDayArr = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  return namesDayArr[new Date(`${date}`).getDay()];
 }
 
 /**
@@ -61,8 +70,20 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  let outputDate;
+  const currentDay = date.getDay();
+  const diffDay = 5 - currentDay;
+  if (diffDay > 0) {
+    outputDate = new Date(
+      date.valueOf() + diffDay * 24 * 60 * 60 * 1000
+    ).toISOString();
+  } else {
+    outputDate = new Date(
+      date.valueOf() + (diffDay + 7) * 24 * 60 * 60 * 1000
+    ).toISOString();
+  }
+  return new Date(outputDate);
 }
 
 /**
@@ -76,8 +97,10 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(
+    new Date(year, month).valueOf() - 24 * 60 * 60 * 1000
+  ).getDate();
 }
 
 /**
